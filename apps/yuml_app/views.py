768b677865ec5a835a7d45a7e46e449355e463a7
CepheidVariable <robertwoodard.usa@gmail.com>
2021-06-23 14:31:33 -0500

    feat: add yuml template

--- apps/yuml_app/views.py
+++ apps/yuml_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
 def yuml(request):
-    return HttpResponse("This will be the dashboard for the yamlbuilder @app.route('/yuml').")
+    # return HttpResponse("This will be the dashboard for the yamlbuilder @app.route('/yuml').")
+    return render(request, 'yuml_index.html')
